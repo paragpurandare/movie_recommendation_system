16864b77eace611fc38b275c616334751bbf7034
paragpurandare <paragpurandare50@gmail.com>
2026-02-23 16:41:41 +0530

    Added enviornment variables for frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "react-hot-toast";
-import {GoogleOAuthProvider} from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={"660489005121-6l3kgh1l7cl2dfrtke6lqcpb80h1o4hq.apps.googleusercontent.com"}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <BrowserRouter>
         <App />
         <Toaster />
